598a5c2fee5c07ac114eaf7ca170964078632a23
nick mccain <uniqueiptv@gmail.com>
2024-01-04 14:27:25 +0200

    bug fixes

--- screens/DetailsScreen.tsx
+++ screens/DetailsScreen.tsx
@@ -1,4 +1,11 @@
-import React, { useEffect, useState, useMemo, useLayoutEffect, useRef } from "react";
+import React, {
+  useEffect,
+  useState,
+  useMemo,
+  useLayoutEffect,
+  useRef,
+  MutableRefObject,
+} from "react";
 import {
   View,
   SafeAreaView,
@@ -8,6 +15,7 @@ import {
   TouchableOpacity,
   Animated,
   ActivityIndicator,
+  RefreshControl,
 } from "react-native";
 import { ListItem } from "react-native-elements";
 import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons/";
@@ -16,8 +24,8 @@ import DateTimeHelper from "../helpers/DateTimeHelper";
 import CopyToClipboardHelper from "../helpers/CopyToClipboardHelper";
 
 import { useSelector, useDispatch } from "react-redux";
-//import { useAppSelector, useAppDispatch } from "../hook/redux"; //types
-import { getPackageInfo } from "../store/parcelSlice";
+//import { useAppSelector, useAppDispatch } from "../hook/redux"; //redux types
+import { getPackageInfo, checkTrackingStatus } from "../store/parcelSlice";
 
 //interfaces
 import { IParcel, IRequestParams } from "../interfaces/parcel";
@@ -31,29 +39,25 @@ const DetailsScreen = ({ route, navigation }) => {
   const trackingId = route.params?.trackingNumber;
   const parcelId = route.params?.id;
   const parcelIndex = route.params?.index;
-  const API_KEY =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiIzMjUwMDk3MC1mZDNhLTExZWQtYjViZC03MTg2YTk3NzRiOTEiLCJzdWJJZCI6IjY0NzMxZjBmNzNkMGJiNTE5NzllNmJmYiIsImlhdCI6MTY4NTI2NjE5MX0.w4gvxSgOOWM1gsUoKcqR-pTJAWPqjSFPmv7TV_-urCs";
   const [parcelInfo, setParcelInfo] = useState<IParcel>();
   const [elementVisible, setElementVisible] = useState<boolean>(false);
   const [responseError, setResponseError] = useState<boolean>(false);
-  const errorMessage =
+  const errorMessage: string =
     "No information about your parcel. We checked all relevant couriers for parcel " +
     trackingId;
   const dispatch = useDispatch<any>();
   //const dispatch = useAppDispatch();
-  const { items, isLoading, error } = useSelector((state: IParcelState) => state.parcel);
-  const {theme, language, location} = useSelector((state: ISettingsState) => state.settings);
+  const { items, isLoading, error } = useSelector(
+    (state: IParcelState) => state.parcel
+  );
+  const { theme, language, location } = useSelector(
+    (state: ISettingsState) => state.settings
+  );
   // Constructing styles for current theme
-  const styles: any = useMemo(() => createStyles(theme), [theme]);
-
+  const styles: any = useMemo(() => createStyles(theme), [theme]); //TODO change <any> type
   const loadingIndicator: boolean = isLoading;
-  const firstUpdate = useRef(true);
-  const fetchData = useRef(false);
-
-  //console.log("DetailsScreen user language: ", language);
-  //console.log("DetailsScreen user location: ", location);
-  //console.log("DetailsScreen state items:", items);
-  //console.log("DetailsScreen parcelInfo:", parcelInfo);
+  const firstUpdate: MutableRefObject<boolean> = useRef(true);
+  const fetchData: MutableRefObject<boolean> = useRef(false);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -80,31 +84,52 @@ const DetailsScreen = ({ route, navigation }) => {
         !fetchData.current
       ) {
         fetchData.current = true;
-        const requestParams: IRequestParams = {
-          trackingId,
-          location,
-          language,
-        };
-        const promise = dispatch(getPackageInfo(requestParams));
-        return () =>  {
-          promise.abort();
-          //alert('exit1');
-        }
+        fetchParcelData();
       }
     }
-
-    //return () => {
-      //if (timeOutId) clearTimeout(timeOutId);
+    return () => {
+      //TODO: abort request when living page
       //promise.abort();
-      //alert('exit2')
-    //};
+    };
   }, [parcelInfo]);
+
+  const fetchParcelData = async () => {
+    const requestParams: IRequestParams = {
+      trackingId,
+      location,
+      language,
+    };
+    const response = await dispatch(getPackageInfo(requestParams));
+    console.log("DeatailsScreen/getPackageInfo/response:", response.payload);
+    if (response.payload?.uuid) {
+      getStatus(response.payload?.uuid);
+    } else {
+      console.log("request getPackageInfo complete!");
+    }
+  };
 
   useEffect(() => {
     updateParcelInfo(parcelId);
   }, [items]);
 
-  const updateParcelInfo = (id) => {
+  const delay = async (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+  const getStatus = async (uuid: string) => {
+    const response = await dispatch(checkTrackingStatus(uuid));
+    console.log(
+      "DetailsScreen/dispatch(checkTrackingStatus)/setTimeout:",
+      response
+    );
+    if (response.payload.done) {
+      console.log("Request complete");
+    } else {
+      console.log("delay 1sec ...");
+      await delay(1000);
+      getStatus(uuid);
+    }
+  };
+
+  const updateParcelInfo = (id: number) => {
     items.find((item) => {
       if (item.id === id) {
         console.log("item by id:", item);
@@ -153,14 +178,18 @@ const DetailsScreen = ({ route, navigation }) => {
           </View>
 
           <View style={styles.status}>
-            <Text style={styles.statusText}>{i18n.t(parcelInfo?.status)}</Text>
+            <Text style={styles.statusText}>
+              {!parcelInfo?.status ? "" : i18n.t(parcelInfo?.status)}
+            </Text>
             <View style={styles.progressBar}>
               <Animated.View
                 style={[
                   StyleSheet.absoluteFill,
                   {
                     backgroundColor:
-                      parcelInfo?.status !== "delivered" ? AppTheme[theme].button : "green",
+                      parcelInfo?.status !== "delivered"
+                        ? AppTheme[theme].button
+                        : "green",
                     width:
                       parcelInfo?.status !== "delivered"
                         ? parcelInfo?.status !== "arrived"
@@ -174,39 +203,68 @@ const DetailsScreen = ({ route, navigation }) => {
           </View>
 
           <View style={styles.loadingIndicator}>
-          {/* {loadingIndicator && <LoadingComponent animation="wave" />} */}
-          <ActivityIndicator
+            {/* {loadingIndicator && <LoadingComponent animation="wave" />} */}
+            {/* <ActivityIndicator
               animating={loadingIndicator}
               size="small"
               color={AppTheme[theme].button}
-            />
+            /> */}
           </View>
 
-          <ScrollView style={styles.scrollView}>
+          <ScrollView
+            refreshControl={
+              <RefreshControl
+                refreshing={isLoading}
+                onRefresh={fetchParcelData}
+                colors={[AppTheme[theme].button]}
+                tintColor={AppTheme[theme].button}
+                //enabled={!isLoading}
+              />
+            }
+            style={styles.scrollView}
+          >
             {parcelInfo?.states &&
               parcelInfo.states.map((state, index) => {
                 return (
-                  <ListItem key={index} bottomDivider containerStyle={{backgroundColor: AppTheme[theme].container}}>
+                  <ListItem
+                    key={index}
+                    bottomDivider
+                    containerStyle={{
+                      backgroundColor: AppTheme[theme].container,
+                    }}
+                  >
                     <MaterialCommunityIcons
                       name="circle-double"
                       size={10}
                       color={
-                        parcelInfo?.status !== "delivered" ? AppTheme[theme].button : "green"
+                        parcelInfo?.status !== "delivered"
+                          ? AppTheme[theme].button
+                          : "green"
                       }
                     />
                     <ListItem.Content>
                       <ListItem.Title
-                        style={{ fontWeight: "800", fontSize: 15, color: AppTheme[theme].title }}
+                        style={{
+                          fontWeight: "800",
+                          fontSize: 15,
+                          color: AppTheme[theme].title,
+                        }}
                       >
                         {state?.status}
                       </ListItem.Title>
-                      <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail" style={{color: AppTheme[theme].title}}>
+                      <ListItem.Subtitle
+                        numberOfLines={1}
+                        ellipsizeMode="tail"
+                        style={{ color: AppTheme[theme].title }}
+                      >
                         <Text style={{ opacity: 0.5, marginRight: 5 }}>
                           {DateTimeHelper.getFormattedDatetime(state?.date)}
                         </Text>
                       </ListItem.Subtitle>
                     </ListItem.Content>
-                    <Text style={{color: AppTheme[theme].title}}>{state?.location}</Text>
+                    <Text style={{ color: AppTheme[theme].title }}>
+                      {state?.location}
+                    </Text>
                   </ListItem>
                 );
               })}
@@ -217,70 +275,70 @@ const DetailsScreen = ({ route, navigation }) => {
   );
 };
 
-const createStyles = (theme) =>
+const createStyles = (theme: string) =>
   StyleSheet.create({
-  container: {
-    backgroundColor: AppTheme[theme].container,
-    height: "100%"
-  },
-  center: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  parcel: {
-    margin: 10,
-    alignItems: "center",
-  },
-  row: {
-    flexDirection: "row",
-  },
-  loading: {
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  options: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    width: "100%",
-    height: 200,
-    backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  status: {
-    marginTop: 13,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  statusText: {
-    fontSize: 18,
-    color: AppTheme[theme].text
-  },
-  progressBar: {
-    width: "90%",
-    height: 10,
-    backgroundColor: "#FFF",
-    borderWidth: 1,
-    borderRadius: 8,
-    borderColor: "#CCC",
-    flexDirection: "row",
-  },
-  copy: {
-    marginLeft: 10,
-    color: AppTheme[theme].button
-  },
-  route: {
-    marginLeft: 15,
-  },
-  scrollView: {
-    //backgroundColor: AppTheme[theme].container,
-  }
-});
+    container: {
+      backgroundColor: AppTheme[theme].container,
+      height: "100%",
+    },
+    center: {
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    parcel: {
+      margin: 10,
+      alignItems: "center",
+    },
+    row: {
+      flexDirection: "row",
+    },
+    loading: {
+      position: "absolute",
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    options: {
+      position: "absolute",
+      bottom: 0,
+      left: 0,
+      width: "100%",
+      height: 200,
+      backgroundColor: "red",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    status: {
+      marginTop: 13,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    statusText: {
+      fontSize: 18,
+      color: AppTheme[theme].text,
+    },
+    progressBar: {
+      width: "90%",
+      height: 10,
+      backgroundColor: "#FFF",
+      borderWidth: 1,
+      borderRadius: 8,
+      borderColor: "#CCC",
+      flexDirection: "row",
+    },
+    copy: {
+      marginLeft: 10,
+      color: AppTheme[theme].button,
+    },
+    route: {
+      marginLeft: 15,
+    },
+    scrollView: {
+      //backgroundColor: AppTheme[theme].container,
+    },
+  });
 
 export default DetailsScreen;
